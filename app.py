4dbc0ec437957eb736be10c4be55a42d727ef575
penguin19coors-star <penguin19coors@gmail.com>
2026-03-06 23:37:51 -0500

    Add extractor arguments for YouTube player client

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ def download_audio():
               "--match-filter", "duration<600",
               "--no-warnings",
               "--no-check-certificates",
+              "--extractor-args", "youtube:player_client=mediaconnect"
               "-o", output_template,
               f"ytsearch1:{query}",
             ],
