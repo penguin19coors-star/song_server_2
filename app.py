0c16952ed300b2a7de62fa8cfd2622830fd95792
penguin19coors-star <penguin19coors@gmail.com>
2026-03-06 22:30:09 -0500

    Change download section duration from 3:45 to 3:30

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def download_audio():
                 "--audio-format", "mp3",
                 "--audio-quality", "10",
                 "--postprocessor-args", "ExtractAudio:-b:a 8k -ac 1",
-                "--download-sections", "*0:00-3:45",
+                "--download-sections", "*0:00-3:30",
                 "--match-filter", "duration<600",
                 "--no-warnings",
                 "--no-check-certificates",
