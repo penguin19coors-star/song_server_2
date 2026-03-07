67a0d7720175bb981481f524902ddff8a5fa5c4b
penguin19coors-star <penguin19coors@gmail.com>
2026-03-07 00:09:49 -0500

    Update audio postprocessor duration to 480 seconds

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def download_audio():
               "--no-playlist",
               "-x",
               "--audio-format", "mp3",
-              "--postprocessor-args", "ffmpeg:-t 360 -b:a 8k -ac 1 -ar 8000",
+              "--postprocessor-args", "ffmpeg:-t 480 -b:a 8k -ac 1 -ar 8000",
               "--match-filter", "duration<600",
               "--no-warnings",
               "--no-check-certificates",
